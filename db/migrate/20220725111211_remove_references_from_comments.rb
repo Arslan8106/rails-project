f2d64ac8ed299ad2b841171cec17d219a35c6f8e
Rails Practice <arslanahmad8106@gmail.com>
2022-07-26 20:06:04 +0500

    Update login Signup interface21

--- db/migrate/20220725111211_remove_references_from_comments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReferencesFromComments < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :comments, :references, :string
-  end
-end
--- db/migrate/20220725111258_remove_article_id_from_comments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveArticleIdFromComments < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :comments, :article_id, :string
-  end
-end
