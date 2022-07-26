3222ceabe14a06fdef2c8c17c4ab69f603322287
Rails Practice <arslanahmad8106@gmail.com>
2022-07-26 19:59:12 +0500

    Update login Signup interface

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,6 +1,6 @@
 class Subscriber < ApplicationRecord
 
-    validates :name, :email, :source, presence: true
+    validates :name, :email,  presence: true
     validates :email, uniqueness: true
 
 end
